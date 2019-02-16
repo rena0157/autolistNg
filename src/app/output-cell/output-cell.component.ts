e8c4fb8b71898a9ddfefa61db4c4b12f0552dce6
Adam Renaud <adamf.renaud@gmail.com>
2019-02-16 09:12:30 -0500

    Adding initial comments

--- src/app/output-cell/output-cell.component.ts
+++ src/app/output-cell/output-cell.component.ts
@@ -1,3 +1,7 @@
+// output-cell component
+// By: Adam Renaud
+// Created: 2019-02-14
+
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
@@ -7,16 +11,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class OutputCellComponent implements OnInit {
 
+    /**
+     * Default Constructor
+     */
     constructor() 
     {
         this.value = "";
         this.placeholder = "";
     }
 
+    /**
+     * Init lifecycle hook
+     */
     ngOnInit() {
     }
 
+    /**
+     * The value of the output cell
+     */
     @Input() public value: string;
 
+    /**
+     * The placeholder value of the cell
+     */
     @Input() public placeholder : string;
 }
